806a298ce9153964868393e15c2be1bc6aeb35f1
Ruslan Shashkov <rshashkov@icloud.com>
2020-02-02 21:07:05 +0200

    Upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { css } from 'emotion';
 
 export const App = () => {
-  return <div className='App'>xxx</div>;
+  return <div css={styles}>xxx</div>;
 };
+
+const styles = css`
+  background-color: red;
+`;

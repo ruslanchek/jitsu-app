27f537d405020e4505766b05b074faddd19c46b4
Ruslan Shashkov <rshashkov@icloud.com>
2020-02-02 11:24:41 +0200

    sss

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const dev = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: dev ? 'development' : 'production',
+  devServer: {
+    contentBase: [path.join(__dirname, 'public')],
+    historyApiFallback: true,
+  },
+  devtool: 'source-map',
+  entry: {
+    app: './src/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build/',
+    filename: '[name].bundle.js',
+  },
+  optimization: {
+    noEmitOnErrors: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV) },
+    }),
+    new MiniCssExtractPlugin({ filename: 'styles.css' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+          {
+            loader: 'linaria/loader',
+            options: { sourceMap: dev },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV !== 'production',
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: dev },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|png|gif|woff|woff2|eot|ttf|svg)$/,
+        use: [{ loader: 'file-loader' }],
+      },
+    ],
+  },
+};

677b1bbaf220344f93d2c92cf7de8c34b27a03be
Ruslan Shashkov <rshashkov@icloud.com>
2020-04-22 16:42:44 +0300

    Upd

--- src/components/screens/MainScreen.tsx
+++ src/components/screens/MainScreen.tsx
@@ -1,54 +1,102 @@
 import React, { FC } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { ScreenLayout } from '../global/ScreenLayout';
-import styles from './MainScreen.module.pcss';
 
 interface IProps extends RouteComponentProps {}
 
 export const MainScreen: FC<IProps> = () => {
   return (
     <ScreenLayout>
-      <div className='max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden'>
-        <input
-          className='bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal'
-          type='email'
-          placeholder='jane@example.com'
-        />
-        <div className='max-w-sm rounded overflow-hidden shadow-lg'>
-          <img className='w-full' src='/img/card-top.jpg' alt='Sunset in the mountains' />
-          <div className='px-6 py-4'>
-            <div className='font-bold text-xl mb-2'>The Coldest Sunset</div>
-            <p className='text-gray-700 text-base'>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
-              eaque, exercitationem praesentium nihil.
-            </p>
+      <div className='fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center'>
+        <div className='fixed inset-0 transition-opacity'>
+          <div className='absolute inset-0 bg-gray-500 opacity-75'></div>
+        </div>
+        <div className='bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full'>
+          <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
+            <div className='sm:flex sm:items-start'>
+              <div className='mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10'>
+                <svg className='h-6 w-6 text-red-600' stroke='currentColor' fill='none' viewBox='0 0 24 24'>
+                  <path
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
+                    d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z'
+                  />
+                </svg>
+              </div>
+              <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>Deactivate account</h3>
+                <div className='mt-2'>
+                  <p className='text-sm leading-5 text-gray-500'>
+                    Are you sure you want to deactivate your account? All of your data will be permanantly removed. This
+                    action cannot be undone.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className='px-6 py-4'>
-            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-              #photography
+          <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
+            <span className='flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto'>
+              <button
+                type='button'
+                className='inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5'>
+                Deactivate
+              </button>
             </span>
-            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2'>
-              #travel
-            </span>
-            <span className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700'>
-              #winter
+            <span className='mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto'>
+              <button
+                type='button'
+                className='inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline transition ease-in-out duration-150 sm:text-sm sm:leading-5'>
+                Cancel
+              </button>
             </span>
           </div>
         </div>
-        <div className='sm:flex sm:items-center px-6 py-4'>
-          <img
-            className='block mx-auto sm:mx-0 sm:flex-shrink-0 h-16 sm:h-24 rounded-full'
-            src='https://randomuser.me/api/portraits/women/17.jpg'
-            alt="Woman's Face"
-          />
-          <div className='mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left'>
-            <p className='text-xl leading-tight'>Erin Lindford</p>
-            <p className='text-sm leading-tight text-gray-600'>Customer Support Specialist</p>
-            <div className='mt-4'>
-              <button className='text-purple-500 hover:text-white hover:bg-purple-500 border border-purple-500 text-xs font-semibold rounded-full px-4 py-1 leading-normal'>
-                Message
-              </button>
+      </div>
+
+      <div className='fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center'>
+        <div className='fixed inset-0 transition-opacity'>
+          <div className='absolute inset-0 bg-gray-500 opacity-75'></div>
+        </div>
+        <div className='bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full'>
+          <div className='bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
+            <div className='sm:flex sm:items-start'>
+              <div className='mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10'>
+                <svg className='h-6 w-6 text-red-600' stroke='currentColor' fill='none' viewBox='0 0 24 24'>
+                  <path
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                    stroke-width='2'
+                    d='M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z'
+                  />
+                </svg>
+              </div>
+              <div className='mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left'>
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>Deactivate account</h3>
+                <div className='mt-2'>
+                  <p className='text-sm leading-5 text-gray-500'>
+                    Are you sure you want to deactivate your account? All of your data will be permanantly removed. This
+                    action cannot be undone.
+                  </p>
+                </div>
+              </div>
             </div>
+          </div>
+          <div className='bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
+            <span className='flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto'>
+              <button
+                type='button'
+                className='inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5'>
+                Deactivate
+              </button>
+            </span>
+            <span className='mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto'>
+              <button
+                type='button'
+                className='inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline transition ease-in-out duration-150 sm:text-sm sm:leading-5'>
+                Cancel
+              </button>
+            </span>
           </div>
         </div>
       </div>

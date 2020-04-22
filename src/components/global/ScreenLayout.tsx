8ca67e3454a5504cc733f434812a522774b3145f
Ruslan Shashkov <rshashkov@icloud.com>
2020-04-22 12:26:02 +0300

    Accept invite

--- src/components/global/ScreenLayout.tsx
+++ src/components/global/ScreenLayout.tsx
@@ -1,15 +1,8 @@
 import React, { FC } from 'react';
-
-import { Header } from './Header';
 import styles from './ScreenLayout.module.pcss';
 
 interface IProps {}
 
 export const ScreenLayout: FC<IProps> = ({ children }) => {
-  return (
-    <main className={styles.ScreenLayout}>
-      <Header />
-      {children}
-    </main>
-  );
+  return <main className={styles.ScreenLayout}>{children}</main>;
 };
--- src/components/screens/MainScreen.tsx
+++ src/components/screens/MainScreen.tsx
@@ -1,12 +1,9 @@
 import React, { FC } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { ScreenLayout } from '../global/ScreenLayout';
-import { useStore } from 'react-stores';
-import { userStore } from '../../eo-core';
 
 interface IProps extends RouteComponentProps {}
 
 export const MainScreen: FC<IProps> = () => {
-  const { profile } = useStore(userStore);
-  return <ScreenLayout>User ID: {profile.id}</ScreenLayout>;
+  return <ScreenLayout>xxx</ScreenLayout>;
 };

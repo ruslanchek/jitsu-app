e6700b7e6830eedbefd7bac417d8664f2eab830f
Ruslan Shashkov <rshashkov@icloud.com>
2020-04-22 12:24:34 +0300

    Accept invite

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,11 @@
 import React, { FC } from 'react';
-import { CoreProvider } from '../core/CoreProvider';
 import { Routes } from './global/Routes';
 import { Wrapper } from './global/Wrapper';
 
 export const App: FC = () => {
   return (
-    <CoreProvider>
-      <Wrapper>
-        <Routes />
-      </Wrapper>
-    </CoreProvider>
+    <Wrapper>
+      <Routes />
+    </Wrapper>
   );
 };

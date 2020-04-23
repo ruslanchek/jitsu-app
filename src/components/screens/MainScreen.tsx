7c46775089bead4ec0baca82082c312fd98a3fa5
Ruslan Shashkov <rshashkov@icloud.com>
2020-04-23 13:00:05 +0300

    Upd

--- src/components/screens/MainScreen.tsx
+++ src/components/screens/MainScreen.tsx
@@ -1,9 +1,18 @@
 import React, { FC } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { ScreenLayout } from '../global/ScreenLayout';
+import { Alert, EAlertType } from '../ui/alert/Alert';
 
 interface IProps extends RouteComponentProps {}
 
 export const MainScreen: FC<IProps> = () => {
-  return <ScreenLayout>x</ScreenLayout>;
+  return (
+    <ScreenLayout>
+      <div className='min-h-screen grid '>
+        <Alert type={EAlertType.Success} details='Details' title='New update available' onClick={() => {}}>
+          How about the forest?
+        </Alert>
+      </div>
+    </ScreenLayout>
+  );
 };
---
+++ src/components/ui/alert/Alert.tsx
@@ -0,0 +1,66 @@
+import React, { FC } from 'react';
+import { RiCheckboxCircleLine, RiInformationLine, RiErrorWarningLine, RiArrowRightLine } from 'react-icons/ri';
+import classnames from 'classnames';
+
+export enum EAlertType {
+  Info,
+  Error,
+  Success,
+}
+
+interface IProps {
+  onClick: () => void;
+  type: EAlertType;
+  title?: string;
+  details?: string;
+}
+
+export const Alert: FC<IProps> = ({ type, title, details, children }) => {
+  let icon;
+
+  switch (type) {
+    case EAlertType.Info: {
+      icon = <RiInformationLine className='text-2xl mr-4 w-6 flex-shrink-0' />;
+      break;
+    }
+    case EAlertType.Error: {
+      icon = <RiErrorWarningLine className='text-2xl mr-4 w-6 flex-shrink-0' />;
+      break;
+    }
+    case EAlertType.Success: {
+      icon = <RiCheckboxCircleLine className='text-2xl mr-4 w-6 flex-shrink-0' />;
+      break;
+    }
+  }
+
+  return (
+    <div
+      className={classnames(
+        'cursor-pointer rounded-lg absolute top-0 flex justify-between px-6 py-4 w-1/2 transition-colors duration-200',
+        {
+          'bg-blue-100 text-blue-600 hover:bg-blue-200 hover:text-blue-700': type === EAlertType.Info,
+          'bg-red-100 text-red-600 hover:bg-red-200 hover:text-red-700': type === EAlertType.Error,
+          'bg-green-100 text-green-600 hover:bg-green-200 hover:text-green-700': type === EAlertType.Success,
+        },
+      )}>
+      <div className='flex-col'>
+        {title && (
+          <strong className='mb-1 flex items-center'>
+            {icon} {title}
+          </strong>
+        )}
+        <div
+          className={classnames('flex justify-between items-center', {
+            'pl-10': title,
+          })}>
+          {!title && icon} {children}
+        </div>
+      </div>
+      {details && (
+        <span className='flex ml-10 items-center'>
+          Details <RiArrowRightLine className='text-xl ml-1' />
+        </span>
+      )}
+    </div>
+  );
+};

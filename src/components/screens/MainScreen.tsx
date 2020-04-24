871e4647e4d1c92c40c7bf43a1524fee5d91f893
Ruslan Shashkov <rshashkov@icloud.com>
2020-04-25 00:02:14 +0300

    ssss

--- src/components/screens/MainScreen.tsx
+++ src/components/screens/MainScreen.tsx
@@ -18,8 +18,8 @@ export const MainScreen: FC<IProps> = () => {
       </Alert> */}
       <br />
       <div className='bg-gray-100 rounded-lg py-4 px-6 w-1/4'>
-        <h4 className='font-bold text-lg text-gray-800 mb-1'>EO Landing</h4>
-        <p className='text-gray-800 text-sm'>Creating new landing for all the luxury</p>
+        <h3 className='font-bold text-xl'>EO Landing</h3>
+        <p className='text-gray-800 text-sm mt-1'>Creating new landing for all the luxury</p>
         <p className='text-gray-600 text-sm mt-4'>26 December, 2019</p>
         <div className='flex mt-2'>
           <div className='mr-1'>
